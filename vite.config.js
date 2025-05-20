255ccb9bc76c3be9202f3edf94c2b330866d273a
LirokWid <baptisterodier@gmail.com>
2025-05-20 19:21:52 +0200

    add vite deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/lirokWid/', // 👈 use your repository name here
+    base: '/Aurelius/', // 👈 use your repository name here
 });
 
 // This is a Vite configuration file that sets the base path for the project for github pages deployment.
